a4c9d3fe4746dc285c3eb07e6e21460b435a346e
gmcavallazzi <gio.cava10@gmail.com>
2022-04-12 16:41:51 +0200

    Add cog-bb distance as input

--- fixedGearCalc.m
+++ fixedGearCalc.m
@@ -6,22 +6,29 @@ function [var,data] = fixedGearCalc(N,n,tire,crank,imperial,cogtobb)
 % with all these quantities visualized. The color scale for the skid patches 
 % refers to how bad the situation is, while for the gear and gain ratios just
 % to how hard they are.
+% 
+% As for now it works with 700c (28") wheels only, but it will be adapted soon
+% to work with other settings.
 %
-% It also gives an approximation of the chain length. It is given assuming 
-% the distance between the center of the cog and of the chain ring = 408
-% mm. This parameter can be modified.
 % 
 % Basic usage: [var,data] = fixedGearCalc(N,n)
 %     N = number of teeth on the chain ring. Default is 49.
 %     n = number of teeth on the rear cog. Default is 17.
 %     
-% Advanced usage: [var,data] = fixedGearCalc(N,n,tire,crank,imperial)
+% Advanced usage: [var,data] = fixedGearCalc(N,n,tire,crank,imperial,cogtobb)
 %     tire = tire size in mm. Default is 28 mm.
 %     crank = crank length in mm. Default is 170 mm.
 %     imperial = switch case for km/h (0), m/s (1) and mi/h (2). Default is 0.
+%     cogtobb  = distance between the center of the rear cog and the bottom
+%                bracket in mm. Default 408 mm.
 %     
 %     Any of these last three additional parameters can be given, it is not 
 %     necessary to list all of them.
+% 
+% It also gives an approximation of the chain length, if the distance between the
+% centers of the rear cog and of the bottom bracket, *cogtobb* is provided. The chain length
+% is evaluated considering an integer number of 1/4" links, it could be useful to
+% check if modifying *cogtobb* and *n* requires adding new links.
 
 addpath('./base')
 
